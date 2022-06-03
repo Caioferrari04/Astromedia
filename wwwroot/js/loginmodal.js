ba5bd4186db62a9c868f571d47ab1c532baf8e2a
Gustavo Ian <gustavoian1790@gmail.com>
2022-06-03 17:13:22 -0300

    fix: deletando código inútil e arrumando modal

--- wwwroot/js/loginmodal.js
+++
@@ -1,8 +0,0 @@
-const modalblock = document.getElementById('modalblock');
-
-window.onclick = function(event) {
-    if (event.target == modalblock) {
-        modalblock.classList.add('is-hidden');
-    }
-}
-
