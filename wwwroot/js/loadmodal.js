7760143847db4d76445d35f9f25e4471cbef4a27
Gustavo Ian <gustavoian1790@gmail.com>
2022-06-05 17:12:34 -0300

    add: adicionando JS

---
+++ wwwroot/js/loadmodal.js
@@ -0,0 +1,17 @@
+const rendermodal = document.getElementById('rendermodal');
+const modalbtn = document.getElementById('modalbtn');
+
+$(modalbtn).click(function() {
+    var url = $(rendermodal).data('url');
+
+    $.get(url, function(data) {
+        $(rendermodal).html(data);
+        $(rendermodal).show();
+    })
+})
+
+$(window).click(function(event) {
+    if (event.target == modalblock) {
+        $(rendermodal).hide();
+    }
+})
