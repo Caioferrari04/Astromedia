1fc8a4671a30cdc1bd47c8945b0d4f8666241f97
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-07-17 14:18:00 -0300

    Criando CRUD de astro

--- Services/AstroService.cs
+++ Services/AstroService.cs
@@ -20,11 +20,41 @@ public class AstroService
 
     public Astro GetById(int id) => _astroContext.Astros.Find(id);
 
+    public List<Astro> GetAll() => _astroContext.Astros.ToList();
+
+    public List<Astro> GetAllByUser(Usuario usuario)
+    {
+        return _astroContext.Astros.Where(astro => astro.Usuarios.Contains(usuario)).ToList();
+    } 
+
+    public void Delete(int id)
+    {
+        var astro = GetById(id);
+
+        _astroContext.Astros.Remove(astro);
+        _astroContext.SaveChanges();
+    }
+
+    public Astro Update(int id, AstroDTO astroDTO)
+    {
+        Astro astro = GetById(id);
+
+        astro.Nome = astroDTO.Nome;
+        astro.Curiosidades = astroDTO.Curiosidades;
+        astro.Foto = astroDTO.Foto;
+        
+        _astroContext.Astros.Update(astro);
+        _astroContext.SaveChanges();
+
+        return astro;
+    }
+
     private Astro ToAstro(AstroDTO astroDTO)
     {
         Astro astro = new Astro();
         astro.Nome = astroDTO.Nome;
         astro.Curiosidades = astroDTO.Curiosidades;
+        astro.Foto = astroDTO.Foto;
 
         return astro;
     }
