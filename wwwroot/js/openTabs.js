009586d58effe81ea7a2083397c2af8db259dc7b
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-08-01 09:23:57 -0300

    Adicionando eventListeners

--- wwwroot/js/openTabs.js
+++ wwwroot/js/openTabs.js
@@ -1,16 +1,20 @@
 const tabs = document.getElementsByClassName("button-nav");
+
+for (const tab of tabs) {
+    tab.addEventListener("click", openTab);
+}
+
 const tabsContent = document.getElementsByClassName("tab-content");
-function openTab(numberTab) {
+
+function openTab() {
     for (let i = 0; i < tabs.length; i++) {
-        if(numberTab == i) {
+        if(this.id === tabs[i].id) {
             tabs[i].classList.add("active");
             tabsContent[i].classList.add("tab-content-active");
         }
-
         else {
             tabs[i].classList.remove("active");
             tabsContent[i].classList.remove("tab-content-active");
         }
-
     }
 }
