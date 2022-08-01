b8acea8a8c7554af18f606ccf12bc37cf383ebc7
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-08-01 09:23:57 -0300

    Adicionando eventListeners

--- wwwroot/js/openTooltip.js
+++ wwwroot/js/openTooltip.js
@@ -1,11 +1,16 @@
-function openTooltip(event) {
-    const img = event.target;
-    let tooltip = img.parentNode.getElementsByClassName('tooltip')[0];
+const itens = document.querySelectorAll('.item img');
+
+for (const item of itens) {
+    item.addEventListener("mouseenter", openTooltip);
+    item.addEventListener("mouseout", closeTooltip);
+}
+
+function openTooltip() {
+    let tooltip = this.parentNode.getElementsByClassName('tooltip')[0];
     tooltip.style.display = "inline";
 }
 
-function closeTooltip(event) {
-    const img = event.target;
-    let tooltip = img.parentNode.getElementsByClassName('tooltip')[0];
+function closeTooltip() {
+    let tooltip = this.parentNode.getElementsByClassName('tooltip')[0];
     tooltip.style.display = "none";
 }
