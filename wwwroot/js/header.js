39dcaf334e0849b007b8485363848744afde63a9
Caioferrari04 <caioferrari0484@gmail.com>
2022-08-03 13:30:10 -0300

    Dropdown estilizado e animado corretamente

--- wwwroot/js/header.js
+++ wwwroot/js/header.js
@@ -2,9 +2,11 @@ function toggleDropdown(dropdown) {
     dropdown.classList.toggle('dropdown-active');
 }
 
-const profile = document.getElementById('profile');
-if (profile != null) {
+const profiles = document.getElementsByClassName('logo-user');
+if (profiles != null) {
     const dropdown = document.getElementById('logged-in');
-    console.log('chegou aqui');
-    profile.addEventListener('click', () => toggleDropdown(dropdown));
+    console.log(profiles);
+    for (const iterator of profiles) {
+        iterator.addEventListener('click', () => toggleDropdown(dropdown));
+    }
 }
