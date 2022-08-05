ec2e5bae49b28d549798a494ef62a7b4b7c3047e
Caioferrari04 <caioferrari0484@gmail.com>
2022-08-04 23:03:02 -0300

    Corrigindo mais bugs de responsividade e dropdown completo

--- wwwroot/js/header.js
+++ wwwroot/js/header.js
@@ -1,12 +1,17 @@
 function toggleDropdown(dropdown) {
     dropdown.classList.toggle('dropdown-active');
+
+    const backdrop = document.getElementById('backdrop');
+    backdrop.classList.toggle('backdrop');
+    backdrop.classList.toggle('is-fixed');
+    backdrop.classList.toggle('is-full-screen');
 }
 
-const profiles = document.getElementsByClassName('logo-user');
-if (profiles != null) {
+const toggles = document.getElementsByClassName('dropdown-toggle');
+if (toggles != null) {
     const dropdown = document.getElementById('logged-in');
-    console.log(profiles);
-    for (const iterator of profiles) {
+    console.log(toggles);
+    for (const iterator of toggles) {
         iterator.addEventListener('click', () => toggleDropdown(dropdown));
     }
 }
