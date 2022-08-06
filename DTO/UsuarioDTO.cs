7c34475b79c0d354da0e3d4e30bda92f09d434bc
Gustavo Ian <gustavoian1790@gmail.com>
2022-08-06 09:52:22 -0300

    fix: Organizando código e melhorando o controle geral das classes no CSS

--- DTO/UsuarioDTO.cs
+++ DTO/UsuarioDTO.cs
@@ -20,6 +20,10 @@ public class UsuarioDTO
 
     public bool LembrarMe { get; set; }
 
+    public bool ConditionTerms { get; set; }
+
+    public bool PrivacyTerms { get; set; }
+
     public List<PostagemDTO> Postagens { get; set; }
 
 }
