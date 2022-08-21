de5a6c4a7688c337a58267916ef849b2ac47ae2b
Gustavo Ian <gustavoian1790@gmail.com>
2022-08-21 11:09:31 -0300

    feat: Construindo área de inserção de texto

--- Controllers/FeedController.cs
+++ Controllers/FeedController.cs
@@ -18,4 +18,6 @@ public class FeedController : Controller {
 
         return View(astro);
     }
+    
+    public IActionResult Postagens() => View();
 }
---
+++ wwwroot/js/textarea-post.js
@@ -0,0 +1,11 @@
+const tx = document.getElementsByTagName("textarea");
+
+for (let i = 0; i < tx.length; i++) {
+  tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;");
+  tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput() {
+  this.style.height = "auto";
+  this.style.height = (this.scrollHeight) + "px";
+}
