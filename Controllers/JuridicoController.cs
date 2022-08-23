5d007f29b12e336a1a8368f88edafe06c49a3bd1
DaikiSakuragi <daikisakuragi@outlook.com>
2022-08-23 10:49:43 -0300

    Novas alterações Termos de Uso

--- Controllers/JuridicoController.cs
+++ Controllers/JuridicoController.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Astromedia.Services;
-using Astromedia.Models;
 
 namespace Astromedia.Controllers;
 
-public class JuridicoController : Controller {
+[AllowAnonymous]
+public class JuridicoController : Controller 
+{
     public IActionResult TermosUso() => View();
 }
