4c9dfbd0e89163d52eff1ad96e31041d75c3ce03
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-10-10 19:35:11 -0300

    Salvando arquivo de imagem no imgur

--- wwwroot/js/submit-post.js
+++ wwwroot/js/submit-post.js
@@ -4,25 +4,23 @@ form.addEventListener("submit",  (event) => {
     handleFormSubmit(event);
 });
 
-function handleFormSubmit(event) {
+async function handleFormSubmit(event) {
     event.preventDefault();
 	
 	let form2 = event.currentTarget;
 
-	let image = document.querySelector("#post-img-preview").getElementsByTagName("img")[0];
-
 	let data = {};
 	data.Texto = form2.elements[0].value;
 	data.DataPostagem = new Date(Date.now());
-	data.Imagem = image == null ? null : image.src;
 	data.UsuarioDTO = null;
+
+
+	const file = document.getElementById('img-up').files[0];
+	file == null ? data.Imagem = null : data.Imagem = await saveImg(file);
 
 	console.log(data)
 	
-	// var texto = form2.elements[0].value;
-    // const formData = new FormData(event.currentTarget);
-
-    fetch(
+    let response = fetch(
 		'https://localhost:7036/Feed/SavePostagem', 
 		{
 			method: "POST",
@@ -30,29 +28,47 @@ function handleFormSubmit(event) {
 			headers: {"Content-type": "application/json; charset=UTF-8"}
 		}
 	)
+
+	if(!response.ok) {
+		handleError()
+	}
 }
 
-//  function postFormDataAsJson(texto) {
-	
-// 	// test.Texto = texto;
-//     // const plainFormData = Object.fromEntries(formData.entries());
-    
-// 	// const formDataJsonString = JSON.stringify(texto);
+async function saveImg(file) {
+	let formData = new FormData();
+	formData.append('image', file);
 
-// 	// const fetchOptions = {
-// 	// 	method: "POST",
-// 	// 	headers: {
-// 	// 		"Content-Type": "application/json",
-// 	// 		Accept: "application/json",
-// 	// 	},
-// 	// 	body: test,
-// 	// };
+	let imgUrl = await fetch('https://api.imgur.com/3/image/', {
+		method: "POST",
+		headers: {
+			Authorization: "Client-ID aca6d2502f5bfd8",
+		},
+		body: formData
+	});
 
-// 	var _data = {};
-// 	_data.Texto = "alex";
-// 	_data.DataPostagem = new Date(Date.now());
-// 	_data.UsuarioDTO = null;
+	if(!imgUrl.ok) {
+		handleError()
+		return null;
+	}
 
-	
+	return await imgUrl.json();
 
-// }
+	// fetch('https://api.imgur.com/3/image/', {
+	// 	method: "POST",
+	// 	headers: {
+	// 		Authorization: "Client-ID aca6d2502f5bfd8",
+	// 	},
+	// 	body: formData
+	// }).then(data => data.json()).then(data => {
+	// 	// img.src = data.data.link
+	// 	img.src = "oi"
+	// 	console.log(data)
+	// });
+}
+
+function handleError() {
+	let textError = document.querySelector(".error-message")
+	if(textError == null) {
+		form.innerHTML += "<p class='error-message'>Erro ao salvar postagem</p>"
+	}
+}
