815717cd0affc2f4a7635152046ecd5341e5ab46
Gustavo Ian <gustavoian1790@gmail.com>
2022-10-12 09:37:34 -0300

    feat/fix: Arrumando algumas coisas da área de comentários

--- wwwroot/js/comment-behav.js
+++ wwwroot/js/comment-behav.js
@@ -1,13 +1,41 @@
-function toggleComment() {
-    const cm = document.querySelector(".comment-input-box");
-    if (cm.style.marginTop === "-128px") {
-        cm.setAttribute("style", "margin-top: -26px !important;");
-        cm.style.transition = ".7s ease-in-out";
-        console.log("comment-box down");
-    } else {
-        cm.setAttribute("style", "margin-top: -128px !important;");
-        cm.style.transition = ".7s ease-in-out";
-        console.log("comment-box up");
-    }
-}
-    
+const cmbtn = document.querySelector(".comment-btn");
+const cmbtns = document.querySelectorAll(".comment-btn");
+let tgg = true;
+let aux;
+
+Array.from(cmbtns).forEach(cmbtn => {
+    cmbtn.addEventListener("click", e => {
+        const cm = document.querySelector(".comment-input-box");
+        const allholder = e.target.closest(".post-box");
+
+        let clone = cm.cloneNode(true);
+
+        if(!(allholder.nextSibling.className == "comment-input-box")) {
+            allholder.after(clone);
+            aux = allholder.nextSibling;
+            verifyMargin();
+            tgg = false;
+        } else {
+            aux = allholder.nextSibling;
+            verifyMargin();
+        }
+
+        console.log(aux);
+        console.log(clone);
+        console.log(tgg);
+
+        function verifyMargin() {
+            if (aux.style.marginTop === "-124px") {
+                console.log("124");
+                aux.setAttribute("style", "margin-top: -26px !important; transition: .7s cubic-bezier(0.72, 0, 0.11, 0.99);");
+            } else if (aux.style.marginTop === "-26px" && tgg) {
+                console.log("tgg");
+                aux.setAttribute("style", "margin-top: -26px !important; transition: .7s cubic-bezier(0.72, 0, 0.11, 0.99);");
+            } else if (aux.style.marginTop === "-26px" && !tgg) {
+                console.log("!!tgg");
+                aux.setAttribute("style", "margin-top: -124px !important; transition: .7s cubic-bezier(0.72, 0, 0.11, 0.99);");
+            }
+        }
+    });
+});
+
