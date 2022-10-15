7504e26b31f294f1f0385c09614364a97925c0c2
Gustavo Ian <gustavoian1790@gmail.com>
2022-10-15 09:23:47 -0300

    fix: Arrumando ativação da área de comentários

--- wwwroot/js/comment-behav.js
+++ wwwroot/js/comment-behav.js
@@ -7,33 +7,30 @@ Array.from(cmbtns).forEach(cmbtn => {
     cmbtn.addEventListener("click", e => {
         const cm = document.querySelector(".comment-input-box");
         const allholder = e.target.closest(".post-box");
-
+        
         let clone = cm.cloneNode(true);
+        // clone.removeAttribute("id"); dar um jeito de esconder a div
 
         if(!(allholder.nextSibling.className == "comment-input-box")) {
             allholder.after(clone);
             aux = allholder.nextSibling;
-            verifyMargin();
+            setTimeout(verifyMargin, 100);
             tgg = false;
         } else {
             aux = allholder.nextSibling;
             verifyMargin();
         }
 
-        console.log(aux);
-        console.log(clone);
-        console.log(tgg);
-
         function verifyMargin() {
             if (aux.style.marginTop === "-124px") {
                 console.log("124");
-                aux.setAttribute("style", "margin-top: -26px !important; transition: .7s cubic-bezier(0.72, 0, 0.11, 0.99);");
+                aux.style.marginTop = "-26px";
             } else if (aux.style.marginTop === "-26px" && tgg) {
                 console.log("tgg");
-                aux.setAttribute("style", "margin-top: -26px !important; transition: .7s cubic-bezier(0.72, 0, 0.11, 0.99);");
+                aux.style.marginTop = "-26px";
             } else if (aux.style.marginTop === "-26px" && !tgg) {
                 console.log("!!tgg");
-                aux.setAttribute("style", "margin-top: -124px !important; transition: .7s cubic-bezier(0.72, 0, 0.11, 0.99);");
+                aux.style.marginTop = "-124px";
             }
         }
     });
