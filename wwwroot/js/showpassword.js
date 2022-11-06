a75a39429fab0454525629c0d6f108e6e30629e0
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-11-06 14:57:58 -0300

    Refatorando arquivo js para apresentar senha

--- wwwroot/js/showpassword.js
+++ wwwroot/js/showpassword.js
@@ -1,20 +1,6 @@
-function showPassword() {
-    const toggleImage = document.getElementById('show-password1');
-    const inputbox = document.getElementById('inputbox-pass1');
-
-    let type = inputbox.getAttribute("type") === "password" ? "text" : "password";
-    inputbox.setAttribute("type", type);
-    
-    if (type === "password") {
-        toggleImage.src = "https://icongr.am/clarity/eye.svg?size=128&color=d2d6dd";
-    } else {
-        toggleImage.src = "https://icongr.am/clarity/eye-hide.svg?size=128&color=d2d6dd";
-    }
-};
-
-function showConfirmation() {
-    const toggleImage = document.getElementById('show-password2');
-    const inputbox = document.getElementById('inputbox-pass2');
+function showPassword(event) {
+    const toggleImage = event.currentTarget.querySelector("img");
+    const inputbox = event.currentTarget.parentNode.querySelector("input");
 
     let type = inputbox.getAttribute("type") === "password" ? "text" : "password";
     inputbox.setAttribute("type", type);
