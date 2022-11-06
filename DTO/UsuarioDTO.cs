4cc5ddacc9aa631290b2212032db0aeec8d2e6a3
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-11-06 14:50:53 -0300

    Adicionando a propriedade nova senha ao dto de usuário

--- DTO/UsuarioDTO.cs
+++ DTO/UsuarioDTO.cs
@@ -8,6 +8,9 @@ public class UsuarioDTO
     
     [EmailAddress]
     public string Email { get; set; }
+    [Display(Name = "Senha atual")]
+    [DataType(DataType.Password)]
+    public string SenhaAtual { get; set; }
 
     [DataType(DataType.Password)]
     public string Senha { get; set; }
@@ -29,4 +32,5 @@ public class UsuarioDTO
     public string FotoPerfil { get; set; }
 
     public List<PostagemDTO> Postagens { get; set; }
+    public bool Atualizar { get; set; }
 }
