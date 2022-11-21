49a84fa0cea29a358a2823d13059a78a8a9ce858
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-11-21 14:31:39 -0300

    Corrigindo edição de astros

--- Controllers/ConfigurationUserController.cs
+++ Controllers/ConfigurationUserController.cs
@@ -181,7 +181,7 @@ public class ConfigurationUserController : Controller
         
         if (validationResult.IsValid)
         {
-            var existingUser = await _userManager.FindByEmailAsync(usuarioDTO.NovoEmail);
+            var existingUser = await _userManager.FindByEmailAsync(_userManager.NormalizeEmail(usuarioDTO.NovoEmail));
             if(existingUser != null)
             {
                 ModelState.AddModelError(string.Empty, "O e-mail fornecido já foi cadastrado.");
@@ -221,7 +221,7 @@ public class ConfigurationUserController : Controller
     public async Task<IActionResult> ConfirmEmail(string email, string newemail, string token)
     {
         var errors = new List<string>();
-        var user = await _userManager.FindByEmailAsync(email);
+        var user = await _userManager.FindByEmailAsync(_userManager.NormalizeEmail(email));
         if (user == null || token == null)
         {
             errors.Add("Token de confirmação de e-mail inválido.");
